ba7038a8da317e14c92233a3c770433fede3a522
retz8 <ekkicb71@gmail.com>
2024-08-06 02:29:58 +0900

    update readme

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-def calculate_sum(a, b):
-    result = a + b
-    return result
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create and index a database")
+    parser.add_argument('dataset_path', type=str, help="Path to the dataset")
+    parser.add_argument('database_type', type=str, choices=['vector', 'graph'], help="Type of the database (vector or graph)")
+    
+    args = parser.parse_args()
+
+    print(args.dataset_path)
